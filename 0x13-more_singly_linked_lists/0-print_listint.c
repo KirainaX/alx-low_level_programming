58bfe83db836554fa48b203ef7eeb8b4f1f67023
KirainaX <bouzridazakaria@email.com>
2023-08-29 00:38:30 -0700

    hmdlh

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,15 +8,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t z = 0;
+	size_t num = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		z++;
+		num++;
 	}
 
-	return (z);
+	return (num);
 }
 
