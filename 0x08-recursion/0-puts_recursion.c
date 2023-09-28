a349a1061ae42d4eb98f304bd82089b1fde5982a
Zakaria <136132497+KirainaX@users.noreply.github.com>
2023-09-28 14:59:00 +0000

    hi

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void _puts_recursion(char *s)
+{
+    if(*s == '\0')
+    {
+        putchar('\n');
+    }
+    else
+    {
+        putchar(*s);
+        _puts_recursion(s + 1);
+    }
